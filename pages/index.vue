83ee857783abd60f54f5c9de062f482fedd14773
leianie <juleseriri@gmail.com>
2019-08-12 20:12:57 +0800

    initial

---
+++ pages/index.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        <span :style="{ color: txtColor1 }">cod</span><span :style="{ color: txtColor2 }">x</span><span :style="{ color: txtColor3 }">.</span><span :style="{ color: txtColor4 }">io</span>
+      </h1>
+      <h5 class="subtitle">
+        💻 soon. 🚀
+      </h5>
+      <!-- <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      txtColor1: '',
+      txtColor2: '',
+      txtColor3: '',
+      txtColor4: '',
+    }
+  },
+  created() {
+    setTimeout(() => this.changeColor(), 1500)
+  },
+  methods: {
+    changeColor() {
+      this.txtColor1 = '#0059bb'
+      setTimeout(()=> this.txtColor2 = '#1287d9', 30)
+      setTimeout(()=> this.txtColor3 = '#f0e53c', 60)
+      setTimeout(()=> this.txtColor4 = '#cf335f', 90)
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Comfortaa';
+  display: block;
+  font-weight: 300;
+  font-size: 80px;
+  color: #181e25;
+  letter-spacing: 1px;
+}
+
+.title span {
+  transition: color 0.2s ease;
+}
+
+.subtitle {
+  font-family: 'Comfortaa';
+  font-weight: 300;
+  /* font-size: 42px; */
+  color: #222b35;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
